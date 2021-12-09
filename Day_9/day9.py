19ca32ebfd507aae516f028c6f83bd18300f5e50
Marvin Waecker <m.waecker@axial3d.com>
2021-12-09 10:53:38 +0000

    day9 part 2

--- Day_9/day9.py
+++ Day_9/day9.py
@@ -16,10 +16,47 @@ def get_neighbours(x, y):
     return {(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)}
 
 def is_low_point(x, y):
-    return all(map.get(pos, 9) > map[(x, y)] for pos in get_neighbours(x, y))
+    neighbors_lower = []
+    for pos in get_neighbours(x,y):
+        if map.get(pos, 9) > map[(x, y)]:   # return 9 if there's no neighbor
+            neighbors_lower.append(True)
+        else:
+            neighbors_lower.append(False)
+    return all(neighbors_lower)
 
 def get_low_points():
-    return [(x, y) for x, y in map if is_low_point(x, y)]
+    lows = []
+    for x, y in map:
+        if is_low_point(x, y):
+            lows.append((x,y))
+    return lows
 
-print("Part 1:")
-print(sum(map[pos] + 1 for pos in get_low_points()))
+
+
+
+#part2
+def get_basin(pos):
+    basin = {pos}
+    follow_path(pos, basin)
+    return basin
+
+def follow_path(pos, basin):
+    p1 = map[pos]
+    for neighbour in get_neighbours(*pos):
+        p2 = map.get(neighbour, 0)          # return 0 if there's no neighbor
+        if p2 > p1 and p2 != 9:
+            basin.add(neighbour)
+            follow_path(neighbour, basin)
+
+
+p = 1
+basins = []
+for pos in get_low_points():
+    basin = get_basin(pos)
+    basins.append(len(basin))
+for basin in sorted(basins)[-3:]:
+    p *= basin
+
+
+print(f"Part 1 : {sum(map[pos] + 1 for pos in get_low_points())}")
+print(f"Part 2 : {p}")
